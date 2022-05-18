aec19e9b0bbd6dc2b2d0e067e74c28318ad385a3
DiegoAraque21 <diego.araque.fernandez@gmail.com>
2022-05-17 22:37:05 -0500

    .

--- situacionProblema.exs
+++ situacionProblema.exs
@@ -1,6 +1,6 @@
 # Actividad Integradora: Resaltador de Sintaxis
 #
-# Diego Araque
+# Diego Araque and Francisco Salcedo
 # 2022 - 05 - 20
 
 defmodule Evidencia do
@@ -11,35 +11,57 @@ defmodule Evidencia do
       |> Enum.map(&readLine/1)
   end
 
-  def readLine(line, htmlLine) do
-    if line = "" do
-
-    else if ()
-
-    end
+  def readLine(line), do: do_readLine(line, "")
+  defp do_readLine("", htmlLine), do: htmlLine
+  defp do_readLine(line, htmlLine) do
+    tuple = getObjectKey(line, htmlLine)
+    do_readLine(elem(tuple, 0), elem(tuple, 1))
+  end
+  defp do_readLine(line, htmlLine) do
+    tuple = getString(line, htmlLine)
+    do_readLine(elem(tuple, 0), elem(tuple, 1))
+  end
+  defp do_readLine(line, htmlLine) do
+    tuple = getNum(line, htmlLine)
+    do_readLine(elem(tuple, 0), elem(tuple, 1))
+  end
+  defp do_readLine(line, htmlLine) do
+    tuple = getBool(line, htmlLine)
+    do_readLine(elem(tuple, 0), elem(tuple, 1))
+  end
+  defp do_readLine(line, htmlLine) do
+    tuple = getWhitespaces(line, htmlLine)
+    do_readLine(elem(tuple, 0), elem(tuple, 1))
   end
 
   def getPuntuation() do
+    Regex.replace()
+  end
+
+  def getObjectKey(line, htmlLine) do
+    lineTemp = line
+    [completeLine, objectKey, puntuation] = Regex.run(~r/^(".*?")(:)/, line)
+    line = elem(String.split_at(lineTemp, String.length(completeLine)),1)
+    htmlLine = "<span class=\"objectKey\">#{objectKey}</span><span class=\"puntuation\">#{puntuation}</span>"
+  end
+
+  def getString(line, htmlLine) do
     Regex.run()
   end
 
-  def getObjectKey() do
+  def getNum(line, htmlLine) do
     Regex.run()
   end
 
-  def getString() do
+  def getBool(line, htmlLine) do
     Regex.run()
   end
 
-  def getNum() do
+  def getWhitespaces(line, htmlLine) do
     Regex.run()
   end
 
-  def getSpace() do
-    Regex.run()
-  end
-
-  def getArray() do
+  def getNull(line, htmlLine) do
     Regex.run()
   end
 end
